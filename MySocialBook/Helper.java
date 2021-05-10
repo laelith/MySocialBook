b9464ac1a51be79dd2a828411b073aaa876d7219
omrum.cetin <cetinomrum@gmail.com>
2021-05-10 14:50:54 +0300

    Fixed merge faults

--- MySocialBook/Helper.java
+++ MySocialBook/Helper.java
@@ -9,39 +9,28 @@ import java.io.FileReader;
 import java.util.Date;
 
 public class Helper
-{
-	public final static String pathToCsv = "C:\\Users\\merve\\users.txt";
-	public static ArrayList <User> FetchUsers() throws IOException, ParseException {
-=======
-import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileReader;
-
-public class Helper
-{
-	public final static String pathToCsv = "users.txt";
-	public static ArrayList <User> FetchUsers()
 	{
->>>>>>> origin/master
-		ArrayList<User> userList = new ArrayList<User>();
-		BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
-		String row = null;
-		while ((row  = csvReader.readLine()) != null) {
-<<<<<<< HEAD
-			String[] data = row.split("\t");
-			String name = data[0];
-			String username = data[1];
-			String password = data[2];
-			SimpleDateFormat dateofBirth = new SimpleDateFormat("MM/dd/yyyy");
-			Date convertedDateofBirth = dateofBirth.parse(data[3]);
-			//String date = dateofBirth.format(convertedDateofBirth);
-			//Trim method is used, because there were some white spaces before the school.
-			String graduatedSchool = (data[4].trim());
-			//This will be created objects of user class.
-			User newUser = new User(name, username, password, convertedDateofBirth, graduatedSchool);
-			userList.add(newUser);
+		public final static String pathToCsv = "users.txt";
+		public static ArrayList <User> FetchUsers() throws IOException, ParseException
+		{
+			ArrayList<User> userList = new ArrayList<User>();
+			BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
+			String row = null;
+			while ((row  = csvReader.readLine()) != null) {
+				String[] data = row.split("\t");
+				String name = data[0];
+				String username = data[1];
+				String password = data[2];
+				SimpleDateFormat dateofBirth = new SimpleDateFormat("MM/dd/yyyy");
+				Date convertedDateofBirth = dateofBirth.parse(data[3]);
+				//String date = dateofBirth.format(convertedDateofBirth);
+				//Trim method is used, because there were some white spaces before the school.
+				String graduatedSchool = (data[4].trim());
+				//This will be created objects of user class.
+				User newUser = new User(name, username, password, convertedDateofBirth, graduatedSchool);
+				userList.add(newUser);
+			}
+			csvReader.close();
+			return userList;
 		}
-		csvReader.close();
-		return userList;
-	}
 }
--- MySocialBook/MySocialBook.java
+++ MySocialBook/MySocialBook.java
@@ -1,48 +1,16 @@
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
-
-public class MySocialBook {
-    
-    public static void main(String[] args) throws IOException, ParseException {
-    	/// Already implemented below part in Helper.FetchUsers() function
-        ArrayList<User> fetchedUsers = Helper.FetchUsers();
-=======
-
 import java.util.Date;
 import java.util.Scanner;
+import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-public class MySocialBook {
-    
-    public static void main(String[] args) throws FileNotFoundException, ParseException {
-    	/// Already implemented below part in Helper.FetchUsers() function
-        Scanner userToAdd = new Scanner(new File("C:\\Users\\merve\\users.txt"));
-        userToAdd.useDelimiter("\t");
-        
-        ArrayList <User> userList = new ArrayList <User>();
-        while(userToAdd.hasNext()) {
-
-            //name<TAB>username<TAB>password<TAB>dateofBirth<TAB>graduatedSchool
-            String name = userToAdd.next();
-            String username = userToAdd.next();
-            String password = userToAdd.next();
-            SimpleDateFormat dateofBirth = new SimpleDateFormat("MM/dd/yyyy");
-            Date convertedDateofBirth = dateofBirth.parse(userToAdd.next());
-            // String date = dateofBirth.format(convertedDateofBirth);
-            //Trim method is used, because there were some white spaces before the school.
-            String graduatedSchool = (userToAdd.nextLine()).trim();
-            //This will be created objects of user class.
-            User newUser = new User(name, username, password, convertedDateofBirth, graduatedSchool);
-            userList.add(newUser);
-        }
-        userToAdd.close();
-
->>>>>>> origin/master
-
+public class MySocialBook
+{
+    public static void main(String[] args) throws IOException, ParseException 
+    {     
+    	ArrayList<User> userList = Helper.FetchUsers();
     }
-
-
 }
 
 
