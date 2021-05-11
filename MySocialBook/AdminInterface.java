a6bbda336a168766bbbdb8f9278913f2617e3d4a
Merve Cetin <mervesafanurcetin@gmail.com>
2021-05-11 15:32:38 +0300

    addUser is updated

--- MySocialBook/AdminInterface.java
+++ MySocialBook/AdminInterface.java
@@ -14,29 +14,26 @@ public class AdminInterface {
 
 	// Adds non existing user
 	// ADDUSER<TAB>name<TAB>userName<TAB>password<TAB>dateofBirth<TAB>schoolGraduate
-	public static ArrayList<User> addUser(ArrayList<User>  userList, String userName, String name, String password, String dateofBirth, String schoolGraduate) throws IOException, ParseException{
+	public static ArrayList<User> addUser(ArrayList<User>  userList, String userName, String name, String password, Date dateofBirth, String schoolGraduate) throws IOException, ParseException{
 
-		// check if user exists in list
+		// checks if user exists in list
 		for (User singleUser : userList) {
 			if (singleUser.getUserName().equals(userName)) {
 				System.out.println("This user already exists.");
 				return null;
 			}
-
-		// do data validation
-
-		// add user
-		
-		// return user list
+		// adds user
+		userList.add(new User (userName, name, password, dateofBirth, schoolGraduate));
+		// returns user list
 		return userList;
 	}
 
 	// Removes existing user
 	public static ArrayList<User> removeUser(ArrayList<User> userList){
-		// check if user exists
+		// checks if user exists
 		for (User singleUser : userList) {
 			if (singleUser.getUserName().equals(userName)) {
-				// remove
+				// removes
 				userList.remove(singleUser);
 			}
 		return userList;
