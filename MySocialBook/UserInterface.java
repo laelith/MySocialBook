08829e4330664b81a34e7cc605410839c1404e10
Merve Cetin <mervesafanurcetin@gmail.com>
2021-05-13 20:19:16 +0300

    addFriend method is updated.

--- MySocialBook/UserInterface.java
+++ MySocialBook/UserInterface.java
@@ -45,67 +45,87 @@ public class UserInterface {
 	public void updateProfile(String name, Date dateOfBirth,String graduatedSchool) throws IOException, ParseException {
 		//User must be logged in before updating the profile.
 		Objects.requireNonNull(this.currentUser);
-		//UPDATEPROFILE<TAB>name<TAB>dateofBirth<TAB>schoolGraduated
-		this.currentUser.getName()=name;
-		this.currentUser.getDateOfBirth()=dateOfBirth;
-		this.currentUser.getGraduatedSchool()=graduatedSchool;
+		if (ifUserLoggedIn()==true){
+			//UPDATEPROFILE<TAB>name<TAB>dateofBirth<TAB>schoolGraduated
+			this.currentUser.setName(name);
+			this.currentUser.setDateOfBirth(dateOfBirth);
+			this.currentUser.setGraduatedSchool(graduatedSchool);
+		}
 	}
 	
 	// Changes password upon current user request
 	public void changePassword(String password) {
 		//CHPASS<TAB>oldPassword<TAB>newPassword
 		Objects.requireNonNull(this.currentUser);
-		this.currentUser.getPassword()=password;
+		if (ifUserLoggedIn()==true) {
+			this.currentUser.setPassword(password);
+		}
 	}
 	
 	// Adds friend to current user
-	public void addFriend() {
+	// ADDFRIEND<TAB>userName
+	public void addFriend(String userName) throws IOException, ParseException {
 		Objects.requireNonNull(this.currentUser);
+		ArrayList<User> userList = Helper.fetchUsers();
+		for (User user: userList){
+			if (ifUserLoggedIn()==true && user.getUserName().equals(userName)){
+				this.currentUser.getFriendList().add(user);
+			}
+		}
 	}
 	
 	// Removes friend if not exists
 	public void removeFriend() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 	
 	// Lists user friends
 	public void listFriends() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 	
 	// Adds posts (maybe required some expansion)
 	public void addPost() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 	
 	// Removing the last post of current user
 	public void removeLastPost() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 	
 	// Showing all posts related with user
 	public void showPosts() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 	
 	// Blocks user from current user if exists
 	public void blockUser() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 	
 	// Unblocks user from current user
 	public void unblockUser() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 	
 	// Lists blocked friends of current user
 	public void listBlockedFriends() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 
 	// Lists blocked users of current user
 	public void listBlockedUsers() {
 		Objects.requireNonNull(this.currentUser);
+
 	}
 
 	// Checks whether User logged in or not
