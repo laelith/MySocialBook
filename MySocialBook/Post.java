3d4d499d0d36d0d6509c752fea064a77d1a39afd
Merve Cetin <mervesafanurcetin@gmail.com>
2021-05-14 15:57:22 +0300

    Post is updated.

--- MySocialBook/Post.java
+++ MySocialBook/Post.java
@@ -10,22 +10,11 @@ public abstract class Post {
     public abstract String display(); //In the case of inclusion a new post type to mysocialbook, designer must specify its printing format. Because of this, Abstract method is used.
 
     public class TextPost extends Post{
-        private String text;
-
         public  String display(){
-            return text
+            return textContent
                     + "Date : " + date
                     + "Location : " + location.getLatitude() + location.getLongitude()
-                     ;
-
-        }
-
-        public String getText() {
-            return text;
-        }
-
-        public void setText(String text) {
-            this.text = text;
+                    + "Friends tagged in this post :" + taggedFriends;
         }
     }
 
@@ -34,7 +23,12 @@ public abstract class Post {
         private Integer imageResolution;
 
         public  String display(){
-            return " ";
+            return textContent
+                    + "Date : " + date
+                    + "Location : " + location.getLatitude() + location.getLongitude()
+                    + "Friends tagged in this post :" + taggedFriends
+                    + "Image: " + imagineFileName
+                    + "Image Resolution: " + imageResolution;
         }
 
         public Integer getImageResolution() {
@@ -57,12 +51,43 @@ public abstract class Post {
     public class VideoPost extends Post{
         private String videoFilename;
         private Double videoDuration;
-        private Integer theMaximumVideoLengthInMinutes=10;
 
         public  String display(){
-            return " ";
+            return textContent
+                    + "Date : " + date
+                    + "Location : " + location.getLatitude() + location.getLongitude()
+                    + "Friends tagged in this post :" + taggedFriends
+                    + "Video: " + videoFilename
+                    + "Video Duration: " + videoDuration + " minutes";
+        }
+
+        public String getVideoFilename() {
+            return videoFilename;
+        }
+
+        public void setVideoFilename(String videoFilename) {
+            this.videoFilename = videoFilename;
+        }
+
+        public Double getVideoDuration() {
+            return videoDuration;
+        }
+
+        public void setVideoDuration(Double videoDuration) {
+            if (videoDuration<10.00){
+                this.videoDuration = videoDuration;
+            }else{
+                System.out.println("Error: Your video exceeds maximum allowed duration of 10 minutes.");
+            }
+        }
+
+        public VideoPost(String videoFilename, Double videoDuration) {
+            this.videoFilename = videoFilename;
+            //videoDuration is checked inside of the set method.
+            setVideoDuration(videoDuration);
         }
     }
+
 
     public String getTextContent() {
         return textContent;
