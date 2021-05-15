db53ba41b9cf63dbc74c814e2f625f824ed6afd1
Merve Cetin <mervesafanurcetin@gmail.com>
2021-05-15 16:43:12 +0300

    There are some problems in My Social Book

--- MySocialBook/MySocialBook.java
+++ MySocialBook/MySocialBook.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
@@ -6,26 +7,50 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class MySocialBook {
-    public static void main(String[] args) throws IOException, ParseException {
-    	Helper.populateUsers();
-    	
-    	//ADDUSER    Adnan    adnan    adnan1    01/01/1991    Selcuk Universitesi
-    	String commandString = "ADDUSER    Adnan    adnan    adnan1    01/01/1991    Selcuk Universitesi";
-    	// Split command into parts
-    	String[] commandParts = commandString.split("\t");
-    	
-    	UserInterface userPanel = new UserInterface();
+	public static void main(String[] args) throws IOException, ParseException {
+		Helper.populateUsers();
 
-		switch (commandParts[0].toUpperCase()) // Means COMMAND in capital case
-    	{
-    	case "ADDUSER":
-    		Date date = new SimpleDateFormat("MM/dd/yyyy").parse(commandParts[4]);
-    		AdminInterface.addUser(commandParts[1], commandParts[2], commandParts[3], date, commandParts[5]);
-    		break;
-    	case "SIGNIN":
-    		userPanel.signIn(commandParts);
-        }
-    }
+		//ADDUSER    Adnan    adnan    adnan1    01/01/1991    Selcuk Universitesi
+		String commandString = "ADDUSER    Adnan    adnan    adnan1    01/01/1991    Selcuk Universitesi";
+		// Split command into parts
+
+		File file = new File("C:\\Users\\merve\\commands.txt");
+		Scanner scanner = new Scanner(file);
+
+		UserInterface userPanel = new UserInterface();
+		AdminInterface adminPanel = new AdminInterface();
+		while (scanner.hasNextLine()) {
+			String[] commandParts = (scanner.nextLine()).split("\t");
+
+			switch (commandParts[0].toUpperCase()) { // Means COMMAND in capital case
+				case "ADDUSER":
+					Date date = new SimpleDateFormat("MM/dd/yyyy").parse(commandParts[4]);
+					AdminInterface.addUser(commandParts[1], commandParts[2], commandParts[3], date, commandParts[5]);
+					break;
+				case "SIGNIN":
+					userPanel.signIn(commandParts[1], commandParts[2]);
+					break;
+				default:
+					System.out.println("Hey");
+//		case "REMOVEUSER":
+//		case "LISTUSERS":
+//		case "UPDATEPROFILE":
+//		case "CHPASS":
+//		case "ADDFRIEND":
+//		case "REMOVEFRIEND":
+//		case "LISTFRIEND":
+//		case "ADDPOST-TEXT":
+//		case "ADDPOST-IMAGE":
+//		case "ADDPOST-VIDEO":
+//		case "REMOVELASTPOST":
+//		case "SHOWPOSTS":
+//		case "BLOCK":
+//		case "SHOWBLOCKEDFRIENDS":
+//		case "UNBLOCK":
+//		case "SHOWBLOCKEDUSERS":
+//		case "SIGNOUT":
+			}
+			scanner.close();
+		}
+	}
 }
-
-
